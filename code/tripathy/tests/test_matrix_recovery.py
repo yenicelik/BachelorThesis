68e95bfa51b1c21793edade596f3fd07075061b9
David <segaslp@gmail.com>
2018-04-15 12:49:49 +0200

    Some more functions and working on recovery

--- code/tripathy/tests/test_matrix_recovery.py
+++ code/tripathy/tests/test_matrix_recovery.py
@@ -7,6 +7,8 @@ from src.t_kernel import TripathyMaternKernel
 from src.t_optimization_functions import t_WOptimizer
 from febo.environment.benchmarks.functions import Rosenbrock
 from febo.environment.benchmarks.functions import Camelback, Parabola
+from src.t_optimizer import TripathyOptimizer
+from src.t_loss import loss, dloss_dW, dK_dW, dloss_ds
 
 from GPy.models.gp_regression import GPRegression
 
@@ -25,10 +27,6 @@ class Metrics(object):
 
         self.tol_mean_diff = 1e-6 # TODO: set this to an ok value
 
-    # TODO: visualization of embedded function
-    # TODO: use utils - cartesian
-    # TODO: optimizers cadidate GridOptimizer
-    # TODO: plot(grid, f(grid))
     # TODO: simulate the real projection by 37-39
 
     def mean_difference_points(self, fnc, fnc_hat, A, A_hat, X):
@@ -89,6 +87,88 @@ class Metrics(object):
         assert len(out) == self.samples
         return all(out)
 
+class TestMatrixRecoveryNaive(object):
+
+    def init(self):
+        self.real_dim = 3
+        self.active_dim = 2
+
+        self.no_samples = 50
+        self.kernel = TripathyMaternKernel(self.real_dim, self.active_dim)
+
+        # Parameters
+        self.sn = 0.1
+        self.W = self.kernel.sample_W()
+
+        self.function = Camelback()
+        self.real_W = np.asarray([
+            [0, 1],
+            [1, 0],
+            [0, 0]
+        ])
+        self.real_W = self.real_W / np.linalg.norm(self.real_W)
+
+        # [[0.9486833]
+        #  [0.31622777]]
+
+        self.X = np.random.rand(self.no_samples, self.real_dim)
+        print(self.X.shape)
+        Z = np.dot(self.X, self.real_W)
+        print(Z.shape)
+        self.Y = self.function._f(Z.T).reshape(-1, 1)
+
+        self.no_tries = 50
+
+    def test_visualize_augmented_sinusoidal_function(self):
+
+        self.init()
+
+        import os
+        if not os.path.exists("./pics/camelback/"):
+            os.makedirs("./pics/")
+            os.makedirs("./pics/camelback/")
+
+        #################################
+        #     TRAIN THE W_OPTIMIZER     #
+        #################################
+
+        Opt = TripathyOptimizer()
+
+        print("Real hidden matrix is: ", self.real_W)
+
+        for j in range(self.no_tries):
+            print("Try number : ", j)
+
+            W_hat = self.kernel.sample_W()
+            self.kernel.update_params(
+                W=W_hat,
+                s=self.kernel.inner_kernel.variance,
+                l=self.kernel.inner_kernel.lengthscale
+            )
+
+            W_hat, sn, l, s = Opt.run_two_step_optimization(self.kernel, self.sn, self.X, self.Y)
+
+            # Create the gp_regression function and pass in the predictor function as f_hat
+            self.kernel.update_params(W=W_hat, l=l, s=s)
+            gp_reg = GPRegression(self.X, self.Y, self.kernel, noise_var=sn)
+            y_hat = gp_reg.predict(self.X)[0].squeeze()
+
+            #################################
+            #   END TRAIN THE W_OPTIMIZER   #
+            #################################
+
+            # Save the W just in case
+            l = loss(
+                self.kernel,
+                W_hat,
+                sn,
+                s,
+                l,
+                self.X,
+                self.Y
+            )
+            np.savetxt("./pics/camelback/Iter_" + str(j) + "__" + "Loss_" + str(l) + ".txt", W_hat)
+
 
 class TestMatrixRecovery(object):
     """
@@ -148,68 +228,66 @@ class TestMatrixRecovery(object):
 
         self.metrics = Metrics(self.no_samples)
 
-    def test_if_function_is_found(self):
-        """
-            Replace these tests by the actual optimizer function!
-        :return:
-        """
-        self.init()
+    # def test_if_function_is_found(self):
+    #     """
+    #         Replace these tests by the actual optimizer function!
+    #     :return:
+    #     """
+    #     self.init()
+    #
+    #     print("Real matrix is: ", self.real_W)
+    #
+    #     all_tries = []
+    #     for i in range(self.tries):
+    #         # Initialize random guess
+    #         W_hat = self.kernel.sample_W()
+    #
+    #         # Find a good W!
+    #         for i in range(self.max_iter):
+    #             W_hat = self.w_optimizer.optimize_stiefel_manifold(W_hat)
+    #
+    #         print("Difference to real W is: ", (W_hat - self.real_W))
+    #
+    #         assert W_hat.shape == self.real_W.shape
+    #
+    #         # TODO: update the gaussian process with the new kernels parameters! (i.e. W_hat)
+    #
+    #         # Create the gp_regression function and pass in the predictor function as f_hat
+    #         gp_reg = GPRegression(self.X, self.Y, self.kernel, noise_var=self.sn)
+    #         res = self.metrics.mean_difference_points(
+    #             fnc=self.function._f,
+    #             fnc_hat=gp_reg.predict,
+    #             A=self.real_W,
+    #             A_hat=W_hat,
+    #             X=self.X
+    #         )
+    #
+    #         all_tries.append(res)
+    #
+    #     print(all_tries)
+    #
+    #     assert np.asarray(all_tries).any()
 
-        print("Real matrix is: ", self.real_W)
-
-        all_tries = []
-        for i in range(self.tries):
-            # Initialize random guess
-            W_hat = self.kernel.sample_W()
-
-            # Find a good W!
-            for i in range(self.max_iter):
-                W_hat = self.w_optimizer.optimize_stiefel_manifold(W_hat)
-
-            print("Difference to real W is: ", (W_hat - self.real_W))
-
-            assert W_hat.shape == self.real_W.shape
-
-            # TODO: update the gaussian process with the new kernels parameters! (i.e. W_hat)
-
-            # Create the gp_regression function and pass in the predictor function as f_hat
-            gp_reg = GPRegression(self.X, self.Y, self.kernel, noise_var=self.sn)
-            res = self.metrics.mean_difference_points(
-                fnc=self.function._f,
-                fnc_hat=gp_reg.predict,
-                A=self.real_W,
-                A_hat=W_hat,
-                X=self.X
-            )
-
-            all_tries.append(res)
-
-        print(all_tries)
-
-        assert np.asarray(all_tries).any()
-
-    def test_if_hidden_matrix_is_found_multiple_initializations(self):
-        self.init()
-
-        print("Real matrix is: ", self.real_W)
-
-        all_tries = []
-
-        for i in range(self.tries):
-            # Initialize random guess
-            W_hat = self.kernel.sample_W()
-
-            # Find a good W!
-            for i in range(self.max_iter):
-                W_hat = self.w_optimizer.optimize_stiefel_manifold(W_hat)
-
-            print("Difference to real (AA.T) W is: ", (W_hat - self.real_W))
-
-            assert W_hat.shape == self.real_W.shape
-            assert not (W_hat == self.real_W).all()
-            res = self.metrics.projects_into_same_original_point(self.real_W, W_hat)
-            all_tries.append(res)
-
-        assert True in all_tries
-
-        # Check if projection is correct
+    # def test_if_hidden_matrix_is_found_multiple_initializations(self):
+    #     self.init()
+    #
+    #     print("Real matrix is: ", self.real_W)
+    #
+    #     all_tries = []
+    #
+    #     for i in range(self.tries):
+    #         # Initialize random guess
+    #         W_hat = self.kernel.sample_W()
+    #
+    #         # Find a good W!
+    #         for i in range(self.max_iter):
+    #             W_hat = self.w_optimizer.optimize_stiefel_manifold(W_hat)
+    #
+    #         print("Difference to real (AA.T) W is: ", (W_hat - self.real_W))
+    #
+    #         assert W_hat.shape == self.real_W.shape
+    #         assert not (W_hat == self.real_W).all()
+    #         res = self.metrics.projects_into_same_original_point(self.real_W, W_hat)
+    #         all_tries.append(res)
+    #
+    #     assert True in all_tries
